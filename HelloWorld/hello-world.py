48eaf26c9b5921371013de046773b1ed9c2b8d85
Stefan Isaksson <stefan.foo.isaksson@gmail.com>
2018-10-31 13:30:21 +0100

    Created new functions including main function

--- HelloWorld/hello-world.py
+++ HelloWorld/hello-world.py
@@ -1,8 +1,24 @@
 import platform
 
-message = f'Hello from Python {platform.python_version()}'
-message_border = '*' * len(message)
+def get_message():
+    return f'Hello from Python {platform.python_version()}'
 
-print(message_border)
-print(message)
-print(message_border)
+	
+def get_border(border_length):
+    return '*' * border_length
+
+	
+def print_message(message, show_border=True):
+    if show_border:
+	    border = get_border(len(message))
+	    print(border + '\n' + message + '\n' + border)
+    else:
+        print(message)
+    
+	
+def main():
+    message = get_message()
+    print_message(message=message)
+
+
+if __name__ == '__main__': main()
